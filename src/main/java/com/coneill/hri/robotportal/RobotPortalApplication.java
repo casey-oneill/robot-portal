01762aefc28b0675c11d1ea9688b233088eeda68
Casey <casey.oneill@unb.ca>
2022-06-23 14:17:41 -0300

    creating spring project

---
+++ src/main/java/com/coneill/hri/robotportal/RobotPortalApplication.java
@@ -0,0 +1,13 @@
+package com.coneill.hri.robotportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coneill/hri/robotportal/RobotPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coneill.hri.robotportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
