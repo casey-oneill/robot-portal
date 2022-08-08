ccf18c114f4a463252ad3c2184699ed52b647bf2
Casey <casey.oneill@unb.ca>
2022-08-08 15:14:26 -0300

    fixing data script

--- data/data.sql
+++ data/data.sql
@@ -8,12 +8,12 @@ SELECT setval('users_seq', (SELECT max(id) from users), true);
 
 -- Create tasks
 INSERT INTO tasks(id, title, description, image_url) VALUES
-	((SELECT nextval('tasks_seq') + 1) + 1, 'Clean Robot Sensors', 'Use the provided cloth to wipe sensors clean.', 'https://picsum.photos/300/300');
+	((SELECT nextval('tasks_seq')), 'Clean Robot Sensors', 'Use the provided cloth to wipe sensors clean.', 'https://picsum.photos/300/300');
 
 SELECT setval('tasks_seq', (SELECT max(id) from tasks), true);
 
 -- Create user task
 INSERT INTO user_tasks(id, user_id, task_id, created_time, is_skipped, is_complete) VALUES
-	((SELECT nextval('user_tasks_seq') + 1), (select max(id) from users), (select max(id) from tasks), current_timestamp, false, false);
+	((SELECT nextval('user_tasks_seq')), (select max(id) from users), (select max(id) from tasks), current_timestamp, false, false);
 
 SELECT setval('user_tasks_seq', (SELECT max(id) from user_tasks), true);
