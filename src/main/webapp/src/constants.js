a1010fb1206ac535c5fb4e22113952aab436a1d1
Casey <casey.oneill@unb.ca>
2022-08-12 14:48:08 -0300

    removing webapp

--- src/main/webapp/src/constants.js
+++
@@ -1,2 +0,0 @@
-export const MAINFORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSe-0vCM5r1ZxpUmfT6QIFNniZDEdklLq7iROS8ThPOS7-z_vQ/viewform?embedded=true";
-export const MAINFORM_UID = "entry.1814644593";
