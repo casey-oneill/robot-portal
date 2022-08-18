0eb2d7bc4335892b86292c7d49c8eaa64343628c
Casey <casey.oneill@unb.ca>
2022-08-18 13:26:18 -0300

    updating data script, formatting task images

--- data/data.sql
+++ data/data.sql
@@ -8,11 +8,21 @@ SELECT setval('users_seq', (SELECT max(id) from users), true);
 
 -- Create tasks
 INSERT INTO tasks(id, title, description, image_url) VALUES
-	((SELECT nextval('tasks_seq')), 'Clean Robot Sensors', 'Use the provided cloth to wipe sensors clean.', 'https://picsum.photos/300/300');
+	((SELECT nextval('tasks_seq')), 'Clean Roomba''s Bin and Filter', 'Follow the instructions below to clean Roomba''s bin and filters.', 'https://github.com/casey-oneill/robot-portal/blob/main/img/task-1.png?raw=true');
 
 SELECT setval('tasks_seq', (SELECT max(id) from tasks), true);
 
--- Create user task
+INSERT INTO tasks(id, title, description, image_url) VALUES
+	((SELECT nextval('tasks_seq')), 'Clean Roomba''s Brushes', 'Follow the instructions below to clean Roomba''s brushes.', 'https://github.com/casey-oneill/robot-portal/blob/main/img/task-2.png?raw=true');
+
+SELECT setval('tasks_seq', (SELECT max(id) from tasks), true);
+
+INSERT INTO tasks(id, title, description, image_url) VALUES
+	((SELECT nextval('tasks_seq')), 'Clean Roomba''s Front Wheel and Cliff Sensors', 'Follow the instructions below to clean Roomba''s front wheel and cliff sensors.', 'https://github.com/casey-oneill/robot-portal/blob/main/img/task-3.png?raw=true');
+
+SELECT setval('tasks_seq', (SELECT max(id) from tasks), true);
+
+-- Create task for admin user
 INSERT INTO user_tasks(id, user_id, task_id, created_time, is_skipped, is_complete) VALUES
 	((SELECT nextval('user_tasks_seq')), (select max(id) from users), (select max(id) from tasks), current_timestamp, false, false);
 
